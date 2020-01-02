8193ad597c29969cfd9907e1f10b80252ebbced4
Manoel Malafaia <mmalafaia@gmail.com>
2020-01-02 10:50:24 -0300

    Deashboard created

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -5,6 +5,7 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 
 import Students from '../pages/Students';
+import Dashboard from '../pages/Dashboard';
 import Plans from '../pages/Plans';
 import Enrollments from '../pages/Enrollments';
 import HelpOrders from '../pages/HelpOrders';
@@ -14,8 +15,9 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
 
+      <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/students" component={Students} isPrivate />
-      <Route path="/Plans" component={Plans} isPrivate />
+      <Route path="/plans" component={Plans} isPrivate />
       <Route path="/enrollments" component={Enrollments} isPrivate />
       <Route path="/helporders" component={HelpOrders} isPrivate />
     </Switch>
--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -21,7 +21,7 @@ export function* signIn({ payload }) {
 
     yield put(signInSuccess(token, user));
 
-    history.push('/students');
+    history.push('/dashboard');
   } catch (err) {
     toast.error('Falha na autenticação, verifique seus dados.');
     yield put(signFailure());
